3e6cb6a74820649a9d35dd91c6664a38b99cbb39
Oleksandr Petryk <o.petryk@dinamicka.com>
2015-12-13 23:45:31 +0200

    myVersion

--- js/script.js
+++ js/script.js
@@ -8,72 +8,75 @@ $(document).ready(function () {
         
     var remain_time = finish_time - current_time;
 
-    
-
     var full_period = finish_time - start_time;
 
-    $('#counter').countdown(
-        {
-            timestamp: (new Date()).getTime() + remain_time
+    var clock;
+
+    $(document).ready(function () {
+        var clock;
+
+        clock = $('.clock').FlipClock({
+            clockFace: 'DailyCounter',
+            autoStart: false,
+            callbacks: {
+                stop: function () {
+                    $('.message').html('The clock has stopped!')
+                }
+            }
         });
 
-    //Create a three progressbar
-
-    var progressbarCircleLeft = $("#progressbarCircleLeft");
-    progressbarCircleLeft.progressbar({
-        value: 0,
-        max: 20
+        clock.setTime(220880);
+        clock.setCountdown(true);
+        clock.start();
 
     });
+    
+    //$('#counter').countdown(
+    //    {
+    //        timestamp: (new Date()).getTime() + remain_time
+    //    });
 
-    var progressbar = $("#progressbar");
-    progressbar.progressbar({
-        value: 0,
-        max: 360
-    });
+    ////Create a three progressbar
 
-    var progressbarCircleRight = $("#progressbarCircleRight");
-    progressbarCircleRight.progressbar({
-        value: 0,
-        max: 20
-    });
+    //var progressbarCircleLeft = $("#progressbarCircleLeft");
+    //progressbarCircleLeft.progressbar({
+    //    value: 0,
+    //    max: 20
 
-    //UpDate Progress bar 
-    var barwidth = (window.screen.width < 600) ? 240 : 380;
-    var oneTickPeriod = Math.floor(full_period / barwidth); // 380 is a progress bar width
+    //});
 
-    var tickNumber = 0;
-    function MoveBar()
-    {
-        var elapsed_time = new Date() - start_time;
-        tickNumber = elapsed_time / oneTickPeriod;
-        SetValueforBar(tickNumber);
-    }
-    setInterval(MoveBar, 100);
+    //var progressbar = $("#progressbar");
+    //progressbar.progressbar({
+    //    value: 0,
+    //    max: 360
+    //});
 
-    //Set a new value for progress bar 
-    function SetValueforBar(number) {
-        progressbar.progressbar("option", "value", number);
+    //var progressbarCircleRight = $("#progressbarCircleRight");
+    //progressbarCircleRight.progressbar({
+    //    value: 0,
+    //    max: 20
+    //});
+
+    ////UpDate Progress bar 
+    //var barwidth = (window.screen.width < 600) ? 240 : 380;
+    //var oneTickPeriod = Math.floor(full_period / barwidth); // 380 is a progress bar width
+
+    //var tickNumber = 0;
+    //function MoveBar()
+    //{
+    //    var elapsed_time = new Date() - start_time;
+    //    tickNumber = elapsed_time / oneTickPeriod;
+    //    SetValueforBar(tickNumber);
+    //}
+    //setInterval(MoveBar, 100);
+
+    ////Set a new value for progress bar 
+    //function SetValueforBar(number) {
+    //    progressbar.progressbar("option", "value", number);
 
 
-        //if (number <= 20 ) {
-        //    progressbarCircleLeft.progressbar("option", "value", number);
-        //}
-        //else if (number > 20 && number < (barwidth-20)) {
-        //    progressbarCircleLeft.progressbar("option", "value", number);
-        //    number = number + 5 - 20;
-        //    progressbar.progressbar("option", "value", number);
-        //}
-        //else if (number > (barwidth - 20)) {
-        //    progressbarCircleLeft.progressbar("option", "value", number);
-        //    progressbar.progressbar("option", "value", number);
-
-        //    number = number - (barwidth - 20);
-
-        //    progressbarCircleRight.progressbar("option", "value", number);
-        //}
         
-    }
+    //}
 
 
     //Redirect to the main page
